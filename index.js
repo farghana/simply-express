e6531c07dbbf0eba8469d6575f90369b28fbca6b
Farghana Hussain <farghana@gmail.com>
2024-01-29 12:30:30 -0500

    Add new endpoint

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res)=>{
     res.send('Welcome to Simply Express!');
 })
 
+app.get('/hello', (req, res)=>{
+    res.send('Hello, World!');
+})
+
 app.listen(PORT, (req, res) => {
     console.log(`Server running on port ${PORT}`);
 })
